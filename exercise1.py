25fdd52f0d39c3b3c961d98725e0da4fb9ab9b13
SebsMacBook <sebastien.dagenais.maniatopoulos@mail.utoronto.ca>
2015-12-16 22:35:14 -0500

    Clicked annotate and I'm not sure what it did so I'm re-committing the code for execise 1

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 """ Assignment 3, Exercise 2, INF1340, Fall, 2015. DBMS
+
 This module performs table operations on database tables
 implemented as lists of lists. """
 
@@ -41,6 +42,7 @@ class UnknownAttributeException(Exception):
 def selection(t, f):
     """
     Perform select operation on table t that satisfy condition f.
+
     Example:
     > R = [["A", "B", "C"], [1, 2, 3], [4, 5, 6]]
     ># Define function f that returns True iff
@@ -48,71 +50,38 @@ def selection(t, f):
     > def f(row): row[-1] > 3
     > select(R, f)
     [["A", "B", "C"], [4, 5, 6]]
+
     """
 
-    """ the call is passed via the f variable
-        so list is sent to filter_employees as an example
-    """
-    results = []
-    for list in t:
-         print list
-         if(f(list) == True):
-           results.append(list)
-
-    return filter(None, results)
+    return []
 
 
 def projection(t, r):
     """
     Perform projection operation on table t
     using the attributes subset r.
+
     Example:
     > R = [["A", "B", "C"], [1, 2, 3], [4, 5, 6]]
     > projection(R, ["A", "C"])
     [["A", "C"], [1, 3], [4, 6]]
+
     """
-    """I append the "columm" id of the calues I want to project"""
-    results = []
-    valuePositions = []
-    i = 0
-    for value in t[0]:
-        if( value in r):
-            valuePositions.append(i)
-        i += 1
 
-    """Once this is done I create a results "table" with only the values selected for projection
-       If that values index is in the valuePositions list it is aded to results"""
-    for list in t:
-      i = 0
-      tempList = []
-      for v in list:
-        if (i in valuePositions):
-            tempList.append(v)
-        i += 1
-      results.append(tempList)
-
-    return results
+    return []
 
 
 def cross_product(t1, t2):
     """
     Return the cross-product of tables t1 and t2.
+
     Example:
     > R1 = [["A", "B"], [1,2], [3,4]]
     > R2 = [["C", "D"], [5,6]]
     [["A", "B", "C", "D"], [1, 2, 5, 6], [3, 4, 5, 6]]
+
+
     """
-    """adding the headers to one another"""
-    result = [t1[0] + t2[0]]
 
-    """doing the cross product of all values. I start my iterator at value 1 in order to ignore the value titles"""
-    i = 1
-    while i < len(t1):
-        j = 1
-        while j <len(t2):
-            result.append(t1[i] + t2[j])
-            j += 1
-        i += 1
+    return []
 
-
-    return result
